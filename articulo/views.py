c26cadbbabd3dd35ff899290d82e5d9024c956a5
Pablo Alegre <pabloalegre98@outlook.com>
2023-07-22 02:26:21 -0300

    'message'

--- articulo/views.py
+++ articulo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Count
 from django.contrib import messages
 from django.views.generic import View, UpdateView, DeleteView
 from .forms import ArticleForm, CategoryForm
-from .models import Articles
+from .models import Articles, Category
 from django.urls import reverse_lazy
 
 
@@ -16,20 +17,32 @@ def home(request):
 class CategoryCreateView(View):
     def get(self, request, *args, **kwargs):
         form = CategoryForm()
+        categories = Category.objects.all()  
         context = {
-            'form': form
+            'form': form,
+            'categories': categories,
         }
-        return render(request, 'profile/articles/category_create.html', context)
+        return render(request, 'category/category_create.html', context)
 
     def post(self, request, *args, **kwargs):
-        if request.method == "POST":
-            form = CategoryForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('articulos:home')
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articulos:create_category')  
+        categories = Category.objects.all()
+        context = {
+            'form': form,
+            'categories': categories,
+        }
+        return render(request, 'category/category_create.html', context)
 
-        context = {}
-        return render(request, 'profile/articles/category_create.html', context)
+
+# Eliminar una categoria
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category/category_delete.html'
+    success_url = reverse_lazy('articulos:create_category')
+    
 
 
 
@@ -81,6 +94,7 @@ class ArtCreateView(View):
 
         context = {}
         return render(request, 'profile/articles/Art_create.html', context)
+    
 
 
 class ArtDetailView(View):
@@ -104,7 +118,7 @@ class ArtUpdateView(UpdateView):
 
     def get_success_url(self):
         pk = self.kwargs['pk']
-        return reverse_lazy('articulos:detail', kwargs={'post_id': pk})
+        return reverse_lazy('articulos:detail_article', kwargs={'post_id': pk})
 
 
 
@@ -112,3 +126,50 @@ class ArtDeleteView(DeleteView):
     model = Articles
     template_name = 'profile/articles/Art_delete.html'
     success_url = reverse_lazy('articulos:home')
+
+
+
+#Mostrar todas las categoria
+class AllCategoriaView(View):
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.annotate(count=Count('articles'))
+        context = {
+            'categories': categories
+        }
+        return render(request, 'category/all_category.html', context)
+    
+
+# Mostrar todos los artículos dependientes de esa categoría
+class AllCategoriaArticlesView(View):
+    def get(self, request, *args, **kwargs):
+        category_id = kwargs.get('category_id')
+        try:
+            category = Category.objects.get(pk=category_id)
+            articles = category.articles_set.all()
+        except Category.DoesNotExist:
+            articles = []
+
+        context = {
+            'category': category,
+            'articles': articles
+        }
+        return render(request, 'category/all_category_articles.html', context)
+    
+
+
+#Mostrar acerca-de
+class AllAboutView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            
+        }
+        return render(request, 'about/about.html', context)
+
+
+#Mostrar contact
+class AllContactView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            
+        }
+        return render(request, 'contact/contact.html', context)
